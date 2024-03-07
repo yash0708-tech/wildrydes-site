2950b60165a906d62f9039c67d39b78dd38edddc
yash0708-tech <yash.b@technmanconsulting.com>
2024-03-07 11:38:42 -0500

    new_configuration

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://w9sy8zvk3k.execute-api.ap-south-1.amazonaws.com/CodeNinja123' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
