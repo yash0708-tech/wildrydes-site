0d9170d428f3d1766d1811ec71f3aa9692fad12e
yash0708-tech <yash.b@technmanconsulting.com>
2024-03-07 10:50:47 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_i2LuXLb2z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4fm07m38q7vnq5065vro3n8bhp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'ap-south-1_UQkeOAQdU', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4petohcsedqdf465ps4ouilqsa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://fs6mwndny4.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
